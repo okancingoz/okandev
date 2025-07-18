2c180782975c4124a8b43182c376eb50b182780e
okancingoz <okanc.ngoz@gmail.com>
2025-07-18 16:18:54 +0300

    fix: fix image problem

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,11 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: "http",
+        hostname: "okandev.onrender.com",
+        pathname: "/uploads/**",
+      },
+      {
         protocol: "https",
         hostname: "api.okandev.me",
         pathname: "/uploads/**",

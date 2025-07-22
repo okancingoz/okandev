96cea1c8eec355ebac930757f609e285c18f6c99
okancingoz <okanc.ngoz@gmail.com>
2025-07-22 14:54:30 +0300

    fix: fix config issues

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Roboto", "Helvetica", "Arial", "sans-serif"],
+        sans: ["Lato", "Helvetica", "Arial", "sans-serif"],
       },
     },
   },
